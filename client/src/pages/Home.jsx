c5340ab65569b9b6cc8e2ae3c271889cc7a2d377
kanishium <155828708+kanishium@users.noreply.github.com>
2025-12-20 23:52:49 +0530

    Initial project setup

---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Banner from '../components/Home/Banner'
+import Hero from '../components/Home/Hero'
+import Features from '../components/Home/Features'
+import Testimonial from '../components/Home/Testimonial'
+
+const Home = () => {
+  return (
+    <div className=''>
+      <Banner/>
+      <Hero/>
+      <Features/>
+    </div>
+  )
+}
+
+export default Home
